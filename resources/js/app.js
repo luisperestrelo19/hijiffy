9311a77bffe5a2dd95eced6fb814770d5596dffe
Luís Perestrelo <luisperestrelo19@gmail.com>
2023-04-24 19:32:53 +0100

    fixed removed imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,6 @@ import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./route";
-import VueSession from "vue-session";
-import { createStore } from "vuex";
 
 
 const app = createApp(App);
